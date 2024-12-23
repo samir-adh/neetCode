6b1df3d67d505d12d8b5563f390e8ea8daf0b24a
Samir Abdallah <samir.abdallah@telecomnancy.net>
2024-12-23 19:53:43 +0100

    Submission accepted

--- BinarySearch/time-based-key-value-store/main.go
+++ BinarySearch/time-based-key-value-store/main.go
@@ -1,12 +1,13 @@
 package timebasedkeyvaluestore
 
+//We define TimeMapEntry structure to access data more easily
 type TimeMapEntry struct {
 	Timestamp int
 	Value     string
 }
 
 type TimeMap struct {
-	entries map[string][]TimeMapEntry
+	entries map[string][]TimeMapEntry // We use a hashamap to access the list of values for each key in o(1)
 }
 
 func Constructor() TimeMap {
@@ -31,42 +32,32 @@ func (this *TimeMap) Set(key string, value string, timestamp int) {
 func (this *TimeMap) Get(key string, timestamp int) string {
 	entryList, keyExists := this.entries[key]
 	if !keyExists { // If the key doesn't exists we return an empty string
-	return ""
-}
-	searchSpace := make([]TimeMapEntry, len(entryList)) 
-	copy(searchSpace, entryList) // We copy the entry list as we don't want to cause any change in the original
-	// if len(searchspace) == 1 {           // case where there is only one element in the entry list
-	// 	entry := searchspace[0]
-	// 	if entry.timestamp < timestamp { // if `entry.timestamp` is greater than `timestamp` we return an empty string
-	// 		return ""
-	// 	} else { // otherwise we can return the value
-	// 		return searchspace[0].value
-	// 	}
-	// }
-
-	/* We use binary search to find the correct timestamp : the entry list
-	is already sorted because timestamp are entered in ascending order */
-	mid := len(searchSpace) / 2 // `mid` is the middle index of the entry list
-	for len(searchSpace) > 1 {
-		if searchSpace[mid].Timestamp == timestamp {
-			return searchSpace[mid].Value
-		} else if searchSpace[mid].Timestamp > timestamp {
-			searchSpace = searchSpace[:mid]
-		} else {
-			/* Notice that we keep the element at index `mid` in `searchSpace`.
-			If the exact timestamp is not present in the entry list this allow
-			us to return the element with the largest timestamp inferior to `timestamp` */
-			searchSpace = searchSpace[mid:]
-		}
-	}
-
-	// Now we check
-	if searchSpace[0].Timestamp <= timestamp {
-		return searchSpace[0].Value
-	} else {
 		return ""
 	}
+	left := 0
+	right := len(entryList) - 1
+	var value string // We use this variable to hold that last acceptable value
 
+	/* 
+	We use binary search to find the correct timestamp : the entry list
+	is already sorted because timestamp are entered in ascending order 
+	*/
+	for right-left >= 0 {
+		mid := (right + left) / 2 // `mid` is the middle index of the search space
+		if timestamp == entryList[mid].Timestamp { // If we find the exact timestamp we can immediatly return the value
+			return entryList[mid].Value
+		} else if timestamp > entryList[mid].Timestamp { 
+			/*
+			If the target timestamp is greater than the current timestamp,
+			the value is acceptable and we can put it in the `value` variable
+			*/
+			value = entryList[mid].Value
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return value
 }
 
 /**
--- BinarySearch/time-based-key-value-store/main_test.go
+++ BinarySearch/time-based-key-value-store/main_test.go
@@ -1,4 +1,4 @@
-/* 
+/*
 Input
 ["TimeMap", "set", "get", "get", "set", "get", "get"]
 [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
@@ -17,10 +17,10 @@ func TestCase1(t *testing.T) {
 	tmap.Set("foo", "bar", 1)
 	output := []string{}
 	output = append(output, tmap.Get("foo", 1))
-	output = append(output,tmap.Get("foo", 3))
+	output = append(output, tmap.Get("foo", 3))
 	tmap.Set("foo", "bar2", 4)
-	output = append(output,tmap.Get("foo", 4))
-	output = append(output,tmap.Get("foo", 5))
+	output = append(output, tmap.Get("foo", 4))
+	output = append(output, tmap.Get("foo", 5))
 
 	expected := []string{
 		"bar",
@@ -29,12 +29,70 @@ func TestCase1(t *testing.T) {
 		"bar2",
 	}
 
-	for index,ouput_element := range(output) {
+	for index, ouput_element := range output {
 		expected_element := expected[index]
 		if ouput_element != expected_element {
-			t.Errorf("Expected : %s ; Got : %s\n",expected_element, ouput_element)
+			t.Errorf("Test case number %d failed : \n	Expected : %s ; Got : %s\n", index, expected_element, ouput_element)
+
 		}
 	}
 
-	
+}
+
+/*
+Last Executed Input
+Use Testcase
+["TimeMap","set","set","set","set","get"]
+[[],["foo","bar",1],["foo", "toilet", 5],["foo", "bucket", 10],["foo","bar2",20],["foo",15]]
+*/
+
+func TestCase2(t *testing.T) {
+	tmap := Constructor()
+	tmap.Set("foo", "bar", 1)
+	tmap.Set("foo", "toilet", 5)
+	tmap.Set("foo", "bucket", 10)
+	tmap.Set("foo", "bar2", 20)
+	output := tmap.Get("foo", 15)
+
+	expected := "bucket"
+
+	if output != expected {
+		t.Errorf("Expected : %s ; Got : %s\n", expected, output)
+
+	}
+}
+
+/*
+	["TimeMap","set","set","get","get","get","get","get"]
+
+[[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+Use Testcase
+Expected
+[null,null,null,"","high","high","low","low"]
+*/
+func TestCase3(t *testing.T) {
+	tmap := Constructor()
+	tmap.Set("love", "high", 10)
+	tmap.Set("love", "low", 20)
+	output := []string{}
+	output = append(output, tmap.Get("love", 5))
+	output = append(output, tmap.Get("love", 10))
+	output = append(output, tmap.Get("love", 15))
+	output = append(output, tmap.Get("love", 20))
+	output = append(output, tmap.Get("love", 25))
+
+	expected := []string{
+		"",
+		"high",
+		"high",
+		"low",
+		"low",
+	}
+
+	for index, ouput_element := range output {
+		expected_element := expected[index]
+		if ouput_element != expected_element {
+			t.Errorf("Test case number %d failed : \n	Expected : \"%s\" ; Got : \"%s\"\n", index+1, expected_element, ouput_element)
+		}
+	}
 }
